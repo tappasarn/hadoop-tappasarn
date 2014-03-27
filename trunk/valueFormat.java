48dd34747cec9a81c030f1ea6c2c4f8d4162482c
time723@gmail.com <time723@gmail.com@3b9ea118-a54b-8af1-a477-0d3e2caf9512>
2014-03-27 14:05:11 +0000

    first version of string matching. Still need to correct format of the final output

---
+++ trunk/valueFormat.java
@@ -0,0 +1,82 @@
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: vaio
+ * Date: 3/27/14
+ * Time: 8:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class valueFormat implements Writable {
+    public int offset;
+    public String fileName;
+
+    public valueFormat(int offset, String fileName){
+        this.offset=offset;
+        this.fileName=fileName;
+
+    }
+    public valueFormat(){
+        this.offset=0;
+        this.fileName=null;
+    }
+    public String getFileName() {
+        return fileName;
+    }
+
+    public long getOffset() {
+        return offset;
+    }
+    @Override
+    public String toString() {
+        return "("+this.fileName + " " + this.offset+")";
+    }
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        //To change body of implemented methods use File | Settings | File Templates.
+
+        String offsetStr = String.valueOf(offset);
+        for(int j=0; j<offsetStr.length(); j++) {
+            dataOutput.write( offsetStr.charAt(j) );
+        }
+
+        dataOutput.writeChar(',');
+
+        for(int j=0; j<fileName.length(); j++) {
+            dataOutput.write(fileName.charAt(j));
+        }
+
+            /*
+            //System.out.println("write"+offset+fileName);
+            for(int i=0;i<offset.toString().length();j++){
+
+                System.out.print(offset.toString().charAt(i));
+                dataOutput.writeChar(offset.toString().charAt(i));
+                if(i==(offset.toString().length()-1)){
+                    dataOutput.writeChar(',');
+                    System.out.print(',');
+                }
+            }
+            for(int j=0;j<fileName.length();j++){
+                System.out.print(fileName.charAt(j));
+                dataOutput.writeChar(fileName.charAt(j));
+            }
+            //dataOutput.writeLong(offset);
+            //dataOutput.writeChars(fileName);
+            */
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        String line = dataInput.readLine();
+        System.out.println("ReadField, in line: " + line); // debug
+        String[] rawIns = line.split(",");
+        System.out.println("value:" + rawIns[1] + "-- offset" + rawIns[0]);
+        this.offset = Integer.parseInt(rawIns[0]);
+        this.fileName = rawIns[1];
+    }
+}
