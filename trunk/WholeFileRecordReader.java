8458469c77ba207f4a4f7526c47b4f0a33e1ace8
time723@gmail.com <time723@gmail.com@3b9ea118-a54b-8af1-a477-0d3e2caf9512>
2014-04-02 08:03:13 +0000

    update algorithm sending array for value for maper not 1g anymore. went well with small file but take very long for large file

--- trunk/WholeFileRecordReader.java
+++ trunk/WholeFileRecordReader.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileSplit;
@@ -19,20 +20,55 @@ import java.io.IOException;
  * Time: 8:55 PM
  * To change this template use File | Settings | File Templates.
  */
-public class WholeFileRecordReader implements RecordReader<NullWritable, Text> {
+public class WholeFileRecordReader implements RecordReader<IntWritable, Text> {
 
     private FileSplit fileSplit;
     private Configuration conf;
+    private IntWritable startingoffset = new IntWritable();
     private Text value = new Text();
-    private boolean processed = false;
+    private int processed = 0;
+    FSDataInputStream in;
     public WholeFileRecordReader(InputSplit inputSplit, JobConf entries){
         fileSplit=(FileSplit)inputSplit;
         conf=(Configuration)entries;
+        Path file = fileSplit.getPath();
+        FileSystem fs = null;
+        try {
+            fs = file.getFileSystem(conf);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
+        in = null;
+        try {
+            in = fs.open(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+
     }
 
     @Override
-    public boolean next(NullWritable nullWritable, Text bytesWritable) throws IOException {
-        if (!processed) {
+    public boolean next(IntWritable nullWritable, Text bytesWritable) throws IOException {
+         if (processed<=(fileSplit.getLength()-2)) {
+               in.seek(processed);
+              System.out.println(processed);
+
+               byte[] contents = new byte[2];
+               in.read(contents, 0, 2);
+              startingoffset.set(processed);
+              String contentString = new String(contents);
+              value.set(contentString);
+              processed = processed+1;
+              return true;
+        }
+        else{
+        IOUtils.closeStream(in);
+        return false;}
+    }
+
+        /*if (!processed) {
             byte[] contents;
             contents = new byte[(int) fileSplit.getLength()];
             Path file = fileSplit.getPath();
@@ -45,7 +81,7 @@ public class WholeFileRecordReader implements RecordReader<NullWritable, Text> {
                 IOUtils.readFully(in, contents, 0, contents.length);
                 String contentString = new String(contents);
                 value.set(contentString);
-                System.out.print("next"+contentString);
+               // System.out.print("next"+contentString);
             } finally {
                 IOUtils.closeStream(in);
             }
@@ -53,11 +89,11 @@ public class WholeFileRecordReader implements RecordReader<NullWritable, Text> {
             return true;
         }
         return false;
-    }
+    }                  */
 
     @Override
-    public NullWritable createKey() {
-        return NullWritable.get();  //To change body of implemented methods use File | Settings | File Templates.
+    public IntWritable createKey() {
+        return startingoffset;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
@@ -77,6 +113,6 @@ public class WholeFileRecordReader implements RecordReader<NullWritable, Text> {
 
     @Override
     public float getProgress() throws IOException {
-        return processed ? 1.0f : 0.0f;
+        return 0;
     }
 }
