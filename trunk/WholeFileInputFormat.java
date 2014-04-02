cc5a51eb1735604a1e3065829d82030b7f449cf9
time723@gmail.com <time723@gmail.com@3b9ea118-a54b-8af1-a477-0d3e2caf9512>
2014-04-02 08:01:55 +0000

    update algorithm sending array for value for maper not 1g anymore. went well with small file but take very long for large file

--- trunk/WholeFileInputFormat.java
+++ trunk/WholeFileInputFormat.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
@@ -13,14 +14,14 @@ import java.io.IOException;
  * Time: 8:53 PM
  * To change this template use File | Settings | File Templates.
  */
-public class WholeFileInputFormat extends FileInputFormat<NullWritable, Text> {
+public class WholeFileInputFormat extends FileInputFormat<IntWritable, Text> {
     @Override
     protected boolean isSplitable(FileSystem fs, Path filename) {
         return false;
     }
 
     @Override
-    public RecordReader<NullWritable, Text> getRecordReader(InputSplit inputSplit, JobConf entries, Reporter reporter) throws IOException {
+    public RecordReader<IntWritable, Text> getRecordReader(InputSplit inputSplit, JobConf entries, Reporter reporter) throws IOException {
         WholeFileRecordReader reader = new WholeFileRecordReader(inputSplit, entries);
         return reader;
     }
